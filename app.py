38432f9ed4ecfa98c895978d47db1fd6e701abbd
Srikhar Shashi D <dasojusrikhar@gmail.com>
2022-08-22 08:17:26 +0530

    model done

---
+++ app.py
@@ -0,0 +1,36 @@
+# Import libraries
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+
+zeroes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+
+app = Flask(__name__)
+# Load the model
+model = pickle.load(open('model1.pkl','rb'))
+
+
+@app.route('/api',methods=['POST'])
+def predict():
+   
+    data = request.get_json(force=True)
+    
+    # Expected data format {"predict":[year,male_percentage,no_of_schools,cost_of_education]}
+    if(data["predict"] == None):
+        return jsonify({"error":"can't find predict key in body"})
+    
+    
+    data["predict"].extend(zeroes)
+    
+    print(data)
+    
+    
+    prediction = model.predict([np.array(data["predict"])])
+    
+    
+    
+    return jsonify({"pass-fail-rate":prediction[0]})
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=False)
