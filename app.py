a3c77e11d1d17fa3c6467a932175b18a14c7586c
Srikhar Shashi D <dasojusrikhar@gmail.com>
2022-08-25 11:46:45 +0530

    works

--- app.py
+++ app.py
@@ -1,62 +1,9 @@
 # Import libraries
-from random import random
+import random as ran
 import numpy as np
 from flask import Flask, request, jsonify
 import pickle
 from flask_cors import CORS
-
-
-
-app = Flask(__name__)
-CORS(app)
-
-# Load the model
-model = pickle.load(open('model1.pkl','rb'))
-
-@app.route('/',methods=['GET'])
-def hehe():
-    return 'hehe'
-
-
-@app.route('/api',methods=['POST'])
-def predict():
-   
-    data = request.get_json(force=True)
-    
-    # Expected data format {"predict":[year,male_percentage,no_of_schools,cost_of_education]}
-    # New Format :: {"predict":[year,state]}
-    if(data["predict"] == None):
-        return jsonify({"error":"can't find predict key in body"})
-    
-
-    
-    
-    state=state_gen(data["predict"][1])
-    
-    year=data["predict"][0]
-    
-    diff=year-2022
-    male_percent=75
-    no_of_schools=random.randint(110000,300000)+(5000*diff)
-    cost_of_education=random.randint(850000,1000000)+(5000*diff)
-    
-    
-    
-    
-    
-    predict_list=[year,male_percent,no_of_schools,cost_of_education]
-    predict_list.extend(state)
-    
-    #trigger
-        
-    
-    prediction = model.predict([np.array(predict_list)])
-
-    return jsonify({"pass-fail-rate":prediction[0]})
-
-
-if __name__ == '__main__':
-    app.run(port=8080, debug=False)
 
 
 def gen_zero(n):
@@ -112,4 +59,53 @@ def state_gen(state):
         
 
 
+app = Flask(__name__)
+CORS(app)
+
+# Load the model
+model = pickle.load(open('model1.pkl','rb'))
+
+@app.route('/',methods=['GET'])
+def hehe():
+    return 'hehe'
+
+
+@app.route('/api',methods=['POST'])
+def predict():
+   
+    data = request.get_json(force=True)
+    
+    # Expected data format {"predict":[year,male_percentage,no_of_schools,cost_of_education]}
+    # New Format :: {"predict":[year,state]}
+    if(data["predict"] == None):
+        return jsonify({"error":"can't find predict key in body"})
+    
+
+    
+    
+    state=state_gen(data["predict"][1])
+    
+    year=data["predict"][0]
+    
+    diff=year-2022
+    male_percent=75
+    no_of_schools=ran.randint(110000,300000)+(5000*diff)
+    cost_of_education=ran.randint(850000,1000000)+(5000*diff)
+    
+    
+    
+    
+    
+    predict_list=[year,male_percent,no_of_schools,cost_of_education]
+    predict_list.extend(state)
+    
+    #trigger
+        
+    
+    prediction = model.predict([np.array(predict_list)])
+
+    return jsonify({"pass-fail-rate":prediction[0]})
+
+
+if __name__ == '__main__':
     app.run(port=8080, debug=False,host="0.0.0.0")
